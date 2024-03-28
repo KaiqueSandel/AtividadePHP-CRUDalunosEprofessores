88cf76346268a8bed2795ee672b0029affb493f7
Andres <128700566+KaiqueSandel@users.noreply.github.com>
2024-03-28 18:33:43 -0300

    Add files via upload

---
+++ AtividadePHP-usandoComposer/Primeiroprojetocomposer/bootstrap.php
@@ -0,0 +1,34 @@
+<?php
+
+require __DIR__."/vendor/autoload.php";
+
+$metodo = $_SERVER['REQUEST_METHOD'];
+$caminho = $_SERVER['PATH_INFO'] ?? '/';
+
+#use Php\Primeiroprojeto\Router
+$r = new Php\Primeiroprojeto\Router($metodo, $caminho);
+
+#ROTAS
+
+$r->get('/exer1/formulario', function(){
+    include("exer1.html");
+});
+
+$r->post('/exer1/resposta', function(){
+    $valor1 = $_POST['valor1'];
+    $valor2 = $_POST['valor2'];
+    $soma = $valor1 + $valor2;
+    return "A soma é: {$soma}";
+});
+
+#ROTAS
+
+$resultado = $r->handler();
+
+if(!$resultado){
+    http_response_code(404);
+    echo "Página não encontrada!";
+    die();
+}
+
+echo $resultado($r->getParams());
